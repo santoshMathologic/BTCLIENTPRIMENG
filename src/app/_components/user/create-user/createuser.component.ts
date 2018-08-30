1f0a1f86be9628a306ff55713008317968b2469f
david <david11sone@gmail.com>
2018-08-30 23:47:44 +0530

    dasdsa

---
+++ src/app/_components/user/create-user/createuser.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Http, Response, RequestOptions, Headers, Request, RequestMethod } from '@angular/http';
+
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './createuser.component.html',
+  styleUrls: ['./createuser.component.css']
+})
+export class CreateUserComponent implements OnInit {
+   
+  
+  constructor(private http: Http) {
+    
+  }
+  ngOnInit() {
+  
+  }
+
+
+
+
+
+
+
+
+}
---
+++ src/app/_components/user/list-user/listuser.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Http, Response, RequestOptions, Headers, Request, RequestMethod } from '@angular/http';
+
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class ListUsersComponent implements OnInit {
+  
+  constructor(){
+
+  }
+  ngOnInit() {
+   
+  }
+
+ 
+
+
+}
